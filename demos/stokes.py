12f310a73ba40e74ab31ed0437178bfc367d6ec4
elepiersan <eleonora@simula.no>
2015-11-19 22:46:22 +0100

    update stokes

--- demos/stokes.py
+++ demos/stokes.py
@@ -12,48 +12,83 @@ from dolfin import *
 #take as input the type of domain
 
 
-def stokes_solver(D, f):
+class StokesSolver(object):
 
-	if D == 1:
-		mesh = UnitIntervalMesh(20)
-	elif D==2:
-	   	mesh = UnitSquareMesh(20, 20)
-	elif D==3:
-		mesh = UnitCubeMesh(20, 20, 20)
+    @staticmethod
+    def default_parameters():
+        return {"D": 2,
+                "f": 0}
 
-	V = VectorFunctionSpace(mesh, "CG", 2)
-	Q = FunctionSpace(mesh, "CG", 1)
-	W = V * Q
+    def __init__(self, params):
+        self.params = params
 
-	# Define boundary conditions
-	#u0 = Expression('1 + x[0]*x[0] + 2*x[1]*x[1]')
+    def __call__(self):
+        D = params["D"]
+        f = params["f"]
 
-	u0 = Constant(0.0)
-	def u0_boundary(x, on_boundary):
-		return on_boundary &&  x[1] > 1.0 - DOLFIN_EPS or x[1] < DOLFIN_EPS
+        if D == 1:
+            mesh = UnitIntervalMesh(20)
+            zero = Constant(0.0)
+        elif D==2:
+            mesh = UnitSquareMesh(20, 20)
+            zero = Constant((0.0,0.0))
+        elif D==3:
+            mesh = UnitCubeMesh(20, 20, 20)
+            zero = Constant((0.0,0.0,0.0))
 
-	bc = DirichletBC(V, u0, u0_boundary)
+        V = VectorFunctionSpace(mesh, "CG", 2)
+        Q = FunctionSpace(mesh, "CG", 1)
+        W = V * Q
 
-	# Define variational problem
-	u = TrialFunction(V)
-	v = TestFunction(V)
-#	f = Constant(-6.0)
-	a = inner(nabla_grad(u), nabla_grad(v))*dx
-	L = f*v*dx
+        # Define boundary conditions
+        #u0 = Expression('1 + x[0]*x[0] + 2*x[1]*x[1]')
+        def Dirichlet_boundary(x, on_boundary):
+            return x[1] > 1.0 - DOLFIN_EPS or x[1] < DOLFIN_EPS
 
-	# Compute solution
-	u = Function(V)
-	solve(a == L, u, bc)
+        bcs = DirichletBC(W.sub(0), zero, Dirichlet_boundary)
 
-	# Plot solution and mesh
-	solution = plot(u)
-	solution.write_png("solution")
-	plot(mesh)
+        (u, p) = TrialFunctions(W)
+        (v, q) = TestFunctions(W)
+        a = inner(grad(u), grad(v))*dx + div(v)*p*dx + q*div(u)*dx
+        L = inner(f, v)*dx
 
-	plot_mesh = plot(mesh)
-	plot_mesh.write_png("mesh")
+        # Form for use in constructing preconditioner matrix
+        b = inner(grad(u), grad(v))*dx + p*q*dx
+
+        # Assemble system
+        A, bb = assemble_system(a, L, bcs)
+
+        # Assemble preconditioner system
+        P, btmp = assemble_system(b, L, bcs)
+
+        # Create Krylov solver and AMG preconditioner
+        solver = KrylovSolver("tfqmr", "amg")
+
+        # Associate operator (A) and preconditioner matrix (P)
+        solver.set_operators(A, P)
+
+        # Solve
+        U = Function(W)
+        solver.solve(U.vector(), bb)
+
+        # Get sub-functions
+        u, p = U.split()
+
+        # Save solution in VTK format
+        solution_u = plot(u)
+        solution_u.write_png("solution_u")
+
+        solution_p = plot(p)
+        solution_p.write_png("solution_p")
+
+        plot_mesh = plot(mesh)
+        plot_mesh.write_png("mesh")
 
 if __name__ == "__main__":
-    D = 2
-    f = Expression("x[0]*x[0]")
-    poisson_solver(D, f)
+
+    params = StokesSolver.default_parameters()
+    params["D"] = 2
+    params["f"] = Expression(("x[0]*x[0]","x[1]*x[1]"))
+
+    solver = StokesSolver(params)
+    solver()
