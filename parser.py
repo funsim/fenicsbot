1e0cacbf8f7dec10892e2c21e66b38d955e26c24
Simon Funke <simon.funke@gmail.com>
2015-11-20 20:05:40 +0100

    Fix Syntax error

--- parser.py
+++ parser.py
@@ -19,7 +19,7 @@ def parse(tweet):
         solver_name = tweet.split(" with ")[0]
         specified_params = tweet.split(" with ")[1].split(" and ")
     except:
-        # if this happens, the tweet should be just 
+        # if this happens, the tweet should be just
         # "@fenicsbot Solve <SolverName>", and we've already excised the preamble
         solver_name = tweet
         specified_params = {}
@@ -29,12 +29,12 @@ def parse(tweet):
     try:
         solver = solvers_by_name[solver_name]
     except:
-        raise ValueError("{} not implemented (yet!)".format(solver_name)
+        raise ValueError, "{} not implemented (yet!)".format(solver_name)
 
     param_dict = solver.default_parameters()
-    
+
     for p in specified_params:
-        # strip() is to not have trouble with spaces - should allow something 
+        # strip() is to not have trouble with spaces - should allow something
         # like "f = expr" as well as "f= expr", "f =expr" or "f=expr"
         parname, parval = p.strip().split("=")
         parname = parname.strip()
@@ -43,7 +43,7 @@ def parse(tweet):
 
 
     solver = solver(param_dict)
-    
+
     return solver
 
 def excise(s):
