956d7e924cd4577834ac2f72fd85b380fde3c6c5
Karl Erik Holter <karl0erik@gmail.com>
2015-11-20 03:10:07 +0100

    Created stokes test stub.

---
+++ tests/test_stokes.py
@@ -0,0 +1,15 @@
+import pytest
+import numpy as np
+
+from fenicsbot.demos import StokesSolver
+
+@pytest.mark.parametrize("mesh", [
+    "UnitInterval",
+    "UnitSquare",
+    "UnitCube",
+    "Dolfin"
+])
+
+## todo
+
+    
