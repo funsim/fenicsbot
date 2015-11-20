6b7c1fdfed2aff0bf1cef1f494f7edd44281941b
Simon Funke <simon.funke@gmail.com>
2015-11-20 15:03:14 +0100

    Uncomment tests - everything passes

--- tests/test_fenicsbot.py
+++ tests/test_fenicsbot.py
@@ -1,7 +1,7 @@
 import pytest, sys
 from fenicsbot.fenicsbot import FEniCSbot
 
-            
+
 SOLUTION_TWEET = "tweeting solution"
 ERROR_TWEET = "tweeting error "
 EXIT_EXCEPTION_MESSAGE = "Exception raised to stop FEniCSbot main loop"
@@ -16,7 +16,7 @@ class DummyTweet():
         #all tweets are recent
         self.id = DummyTweet.id_counter
         DummyTweet.id_counter += 1
-        self.created_at_in_seconds = sys.maxint 
+        self.created_at_in_seconds = sys.maxint
 
         class DummyUser():
             def __init__(self):
@@ -25,12 +25,12 @@ class DummyTweet():
         self.user = DummyUser()
 
 class DummyTwitterApi():
-    """ 
-    dummy Twitter API for tricking FEniCSbot 
+    """
+    dummy Twitter API for tricking FEniCSbot
     into believing it is doing something meaningful
     """
     def __init__(self, mentions_batches):
-        self.tweets = []    
+        self.tweets = []
         self.mentions = map(lambda l: map(DummyTweet, l),
                             mentions_batches)
         # print self.mentions
@@ -57,32 +57,32 @@ class DummyTwitterApi():
             mentions = self.mentions[0]
             self.mentions = self.mentions[1:]
             return mentions
-        else:          
+        else:
             return []
 
 def dummy_bot(list_of_tweet_text_lists):
     """
     Creates a dummy FEniCSbot which will receive batches of tweets
-    with text given by list_of_tweet_text_lists, then exit when 
+    with text given by list_of_tweet_text_lists, then exit when
     looking for the next batch of mentions
     """
     def loop_stopper(bot):
         return bot.api.is_empty()
-        
-    return FEniCSbot(DummyTwitterApi(list_of_tweet_text_lists), 
-                     sleep_time=SLEEP_TIME, 
+
+    return FEniCSbot(DummyTwitterApi(list_of_tweet_text_lists),
+                     sleep_time=SLEEP_TIME,
                      break_loop=loop_stopper)
 
 
 
 def rebatch(l, l_sizes):
     """
-    Helper method for running a test several times 
+    Helper method for running a test several times
     with different batch sizes.
-    
-    Resizes a plain list into a list of lists with the 
-    same elements, but with lengths given in l_sizes. 
-    
+
+    Resizes a plain list into a list of lists with the
+    same elements, but with lengths given in l_sizes.
+
     Example:
     l = [1, 2, 3, 4, 5] becomes:
       --[[1, 2], [3, 4], [5]] with l_sizes = [2, 2, 1]
@@ -93,7 +93,7 @@ def rebatch(l, l_sizes):
     # print l, l_sizes
     if len(l) != sum(l_sizes):
         raise Exception("User counting error")
-    
+
     i = 0
     return_list = [[] for batch_size in l_sizes]
     for j in range(len(l_sizes)):
@@ -113,7 +113,7 @@ def rebatch(l, l_sizes):
 ])
 def test_rebatch(l_sizes, l_ret):
     ## Has testing gone too far?
-    
+
     l = range(1, 6)
     l_rebatched = rebatch(l, l_sizes)
     assert len(l_rebatched) == len(l_ret)
@@ -122,7 +122,7 @@ def test_rebatch(l_sizes, l_ret):
         for j in range(len(l_ret[i])):
             assert l_ret[i][j] == l_rebatched[i][j]
 
-    
+
 
 def run_test_tweets(list_of_tweet_batches, list_of_success_statuses):
     """
@@ -139,10 +139,10 @@ def run_test_tweets(list_of_tweet_batches, list_of_success_statuses):
     #     print bot.api.is_empty
     #     assert bot.api.is_empty
 
-   
+
     # verify that all messages were "delivered" to fenicsbot
 
-    
+
     tweet_no = 0
     # print list_of_tweet_batches
     # print bot.api.tweets
@@ -157,46 +157,46 @@ def run_test_tweets(list_of_tweet_batches, list_of_success_statuses):
 
 
 
-# @pytest.mark.parametrize("batch_sizes", [
-#     [2, 2, 1], 
-#     [4, 1],
-#     [5],
-#     [1, 1, 1, 1, 1]
-# ])
-# def test_good_tweets(batch_sizes):
-#     # Checks that these tweets are parsed without exception 
-#     # even when given in batches of various sizes
-#     tweets = [
-#         "@fenicsbot Solve Poisson with f=1",
-#         "@fenicSbot Solve Poisson",
-#         "@FEniCSbot Solve Poisson with f=1 and domain=UnitSquare",
-#         "@feNicsBot Solve Stokes",
-#         "@feNicsBot Solve Stokes"
-#     ]
-#     statuses = [True]*5
+@pytest.mark.parametrize("batch_sizes", [
+    [2, 2, 1],
+    [4, 1],
+    [5],
+    [1, 1, 1, 1, 1]
+])
+def test_good_tweets(batch_sizes):
+    # Checks that these tweets are parsed without exception
+    # even when given in batches of various sizes
+    tweets = [
+        "@fenicsbot Solve Poisson with f=1",
+        "@fenicSbot Solve Poisson",
+        "@FEniCSbot Solve Poisson with f=1 and domain=UnitSquare",
+        "@feNicsBot Solve Stokes",
+        "@feNicsBot Solve Stokes"
+    ]
+    statuses = [True]*5
 
-#     run_test_tweets(rebatch(tweets, batch_sizes), 
-#                 rebatch(statuses, batch_sizes))
-
-
-# @pytest.mark.parametrize("tweet", [
-#     "@fenisbot Solve Poisson with f=1",
-#     "Solve Poisson",
-#     "@fenicsbot Poisson with g=1 and D=2",
-#     "@fenicsbot Solve with f=1 and n=2",
-#     "@fenicsbot Solve",
-#     "@fenicsbot ",
-#     "@FEnCSbot Solve Poisson with f=1 and D=2",
-# ])
-# def test_bad_tweet(tweet):
-#     batches = [[tweet]]
-#     statuses = [[False]]
-#     run_test_tweets(batches, statuses)
+    run_test_tweets(rebatch(tweets, batch_sizes),
+                rebatch(statuses, batch_sizes))
 
 
-# def test_single_good_tweet():
-#     batches = [["@fenicsbot Solve Poisson with f=1"]]
-#     statuses = [[True]]
-#     run_test_tweets(batches, statuses)
+@pytest.mark.parametrize("tweet", [
+    "@fenisbot Solve Poisson with f=1",
+    "Solve Poisson",
+    "@fenicsbot Poisson with g=1 and D=2",
+    "@fenicsbot Solve with f=1 and n=2",
+    "@fenicsbot Solve",
+    "@fenicsbot ",
+    "@FEnCSbot Solve Poisson with f=1 and D=2",
+])
+def test_bad_tweet(tweet):
+    batches = [[tweet]]
+    statuses = [[False]]
+    run_test_tweets(batches, statuses)
+
+
+def test_single_good_tweet():
+    batches = [["@fenicsbot Solve Poisson with f=1"]]
+    statuses = [[True]]
+    run_test_tweets(batches, statuses)
 
 
