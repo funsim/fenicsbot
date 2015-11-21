c6e06428870f8819faf1c5cde068e8090937f537
Simon Funke <simon.funke@gmail.com>
2015-11-21 22:11:24 +0100

    Skip test_tweet.py script when running py.test

--- test_tweet.py
+++ test_tweet.py
@@ -3,8 +3,9 @@
 import sys
 from parser import parse
 
-tweet = sys.argv[1]
-#tweet = "@fenicsbot solve Poisson in domain UnitSquare with f=1/(x+0.5)"
-solver = parse(tweet)
-solver.solve()
-print solver.plot()
+if __name__ == "__main__":
+    tweet = sys.argv[1]
+    #tweet = "@fenicsbot solve Poisson in domain UnitSquare with f=1/(x+0.5)"
+    solver = parse(tweet)
+    solver.solve()
+    print solver.plot()
