fa54c64df3bc2c203ac86b989667cadc7a1fc858
Simon Funke <simon.funke@gmail.com>
2015-11-21 22:04:44 +0100

    test_tweet now takes tweet message as command line argument

--- test_tweet.py
+++ test_tweet.py
@@ -1,8 +1,10 @@
 """ A simple test script to simulate an handling request """
 
+import sys
 from parser import parse
 
-tweet = "@fenicsbot solve Poisson in domain UnitSquare with f=1/(x+0.5)"
+tweet = sys.argv[1]
+#tweet = "@fenicsbot solve Poisson in domain UnitSquare with f=1/(x+0.5)"
 solver = parse(tweet)
 solver.solve()
 print solver.plot()
